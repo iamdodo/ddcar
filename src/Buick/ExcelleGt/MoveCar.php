8bf61de59e7876cb6eb66c812dfafd04d2666517
idodo <idodo@idodo.local>
2018-05-29 11:17:57 +0800

    in order to build my packagist

--- src/Buick/ExcelleGt/MoveCar.php
+++ src/Buick/ExcelleGt/MoveCar.php
@@ -12,6 +12,6 @@ namespace Buick\ExcelleGt;
 class MoveCar
 {
     function move() {
-        echo 'move my car!';
+        echo 'move my ExcelleGt car!';
     }
 }
--- src/Buick/Verano/MoveCar.php
+++ src/Buick/Verano/MoveCar.php
@@ -12,6 +12,6 @@ namespace Buick\Verano;
 class MoveCar
 {
     function move() {
-        echo 'move my car!';
+        echo 'move my verano car!';
     }
 }
