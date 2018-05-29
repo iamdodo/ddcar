9273ae4fdb686052c58c641734e3d05cda473319
idodo <idodo@idodo.local>
2018-05-29 09:53:33 +0800

    in order to build my packagist

---
+++ src/Buick/ExcelleGt/MoveCar.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: idodo
+ * Date: 2018/5/29
+ * Time: 上午9:43
+ */
+
+namespace Buick\ExcelleGt;
+
+
+class MoveCar
+{
+    function move() {
+        echo 'move my car!';
+    }
+}
---
+++ src/Buick/Verano/MoveCar.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: idodo
+ * Date: 2018/5/29
+ * Time: 上午9:43
+ */
+
+namespace Buick\Verano;
+
+
+class MoveCar
+{
+    function move() {
+        echo 'move my car!';
+    }
+}
